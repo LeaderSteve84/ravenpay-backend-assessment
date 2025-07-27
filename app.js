dcf77c952a8094917b119eafd6ee9a89adba9be5
stephen_adah <mailstephenreal@gmail.com>
2025-07-28 00:10:41 +0100

    MOD: Implemented morgan for loggings

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require("morgan");
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -6,6 +7,7 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev")); // Logs requests in 'dev' format
 
 app.use('/api/auth', authRoutes);
 
