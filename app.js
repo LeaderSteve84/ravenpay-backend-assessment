2019c1809110b7b1ac3f2a1712b312f4ebdb0fd6
stephen_adah <mailstephenreal@gmail.com>
2025-07-28 08:43:11 +0100

    MOD: Mount webhook route

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authRoutes = require('./routes/auth');
+const webhookRoutes = require('./routes/webhook');
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev")); // Logs requests in 'dev' format
 
 app.use('/api/auth', authRoutes);
+app.use('/api/webhook', webhookRoutes);
 
 module.exports = app;
